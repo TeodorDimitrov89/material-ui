cec16dcd94d5e277057b9c963a3ad82440572fda
Teodor Dimitrov <teodor@kipo.bg>
2021-08-15 19:12:19 +0300

    general layout component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Notes from './pages/Notes';
 import Create from './pages/Create';
 // import blueGrey from '@material-ui/core/colors/';
 import cyan from '@material-ui/core/colors/cyan';
 import purple from '@material-ui/core/colors/purple';
-import {
-  createTheme,
-  ThemeProvider,
-} from '@material-ui/core';
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import Layout from './components/Layout';
 
 const theme = createTheme({
   palette: {
@@ -35,14 +29,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route exact path='/'>
-            <Notes />
-          </Route>
-          <Route path='/create'>
-            <Create />
-          </Route>
-        </Switch>
+        <Layout>
+          <Switch>
+            <Route exact path='/'>
+              <Notes />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+          </Switch>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
---
+++ src/components/Layout.js
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+
+const useStyles = makeStyles({
+  page: {
+    background: '#f9f9f9',
+    width: '100%',
+  },
+});
+
+export default function Layout({ children }) {
+  const classes = useStyles();
+  return (
+    <div>
+      {/* App bar */}
+
+      {/* Side drawer */}
+      <div className={classes.page}>{children}</div>
+    </div>
+  );
+}
